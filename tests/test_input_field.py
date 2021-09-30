f155b08ed5cfc147c6de0cd9ab3447cbe2bb8efb
ask1909 <k.bhalerao.1909@gmail.com>
2021-09-30 12:06:44 +0530

    Modified test files to include path modification

--- tests/test_input_field.py
+++ tests/test_input_field.py
@@ -1,5 +1,7 @@
 import pytest
 import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from source.page import *
 from source.input_component import *
 from source.logger import *
